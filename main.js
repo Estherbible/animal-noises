661553a1d10ba5e3af30304945d4958fa160e055
Estherbible <115696413+Estherbible@users.noreply.github.com>
2023-06-05 18:10:00 +1000

    Add files via upload

--- main.js
+++ main.js
@@ -1,13 +1,37 @@
-// https://teachablemachine.withgoogle.com/models/pPYomPuLC/model.json
+
 function start(){
     navigator.mediaDevices.getUserMedia({audio: true})
-    classifier=ml5.soundClassifier("https://teachablemachine.withgoogle.com/models/pPYomPuLC/model.json",modelready)
+    classifier=ml5.soundClassifier("https://teachablemachine.withgoogle.com/models/fqkmDhWd3/model.json",modelready)
 }
 
 function modelready(){
   console.log("model is ready");
   classifier.classify(gotresult)
 }
-function gotresult(error,result){
-console.log(result);
+function gotresult(error, result) {
+  if (error) {
+    console.error(error);
+  } else {
+    console.log(result);
+    r = Math.floor(Math.random() * 255) + 1
+    g = Math.floor(Math.random() * 255) + 1
+    b = Math.floor(Math.random() * 255) + 1
+    document.getElementById("label").innerHTML = "i can hear " + result[0].label
+    document.getElementById("accuracy").innerHTML = "confidence " + (result[0].confidence * 100).toFixed(2) + "%"
+    document.getElementById("label").style.color = "rgb(" + r + "," + g + "," + b + ")"
+    document.getElementById("accuracy").style.color = "rgb(" + r + "," + g + "," + b + ")"
+    console.log("rgb(" + r + "," + g + "," + b + ")");
+    img1=document.getElementById("picture");
+    
+    if (result[0].label=="dog") {
+      img1.src="dog.png"
+
+    } else if(result[0].label=="cat"){
+      img1.src="cat.png"
+
+    } else if(result[0].label=="Background Noise"){
+      img1.src="ear.jpg"
+      
+    }
+  }
 }
